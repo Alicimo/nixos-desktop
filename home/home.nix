cfcb6626a82c750be2c8d1f18b84f89bcc6ea348
Alistair Tiefenbacher <contact@alistair-martin.com>
2024-04-02 22:46:13 +0200

    Added guake drop-down terminal emulator

--- home/home.nix
+++ home/home.nix
@@ -7,32 +7,33 @@
     ./gnome.nix
     ./vim.nix
     ./vscode.nix
+    ./guake.nix
   ];
   home.username = "alistair";
   home.homeDirectory = "/home/alistair";
   home.packages = with pkgs; [
-      thunderbird
-      vlc
-      libreoffice
-      rstudio
-      anki
-      telegram-desktop
+    thunderbird
+    vlc
+    libreoffice
+    rstudio
+    anki
+    telegram-desktop
 
-      htop
-      atop
-      iotop
-      unzip
-      wget
-      curl
-      imagemagick
-      tree
-      cifs-utils
-      pkgs.unstable.llama-cpp
+    htop
+    atop
+    iotop
+    unzip
+    wget
+    curl
+    imagemagick
+    tree
+    cifs-utils
+    pkgs.unstable.llama-cpp
 
-      handbrake
-      makemkv
-      (abcde.overrideAttrs (oldAttrs: { buildInputs = oldAttrs.buildInputs ++ [ pkgs.perlPackages.IOSocketSSL ]; }))
-      flac
+    handbrake
+    makemkv
+    (abcde.overrideAttrs (oldAttrs: { buildInputs = oldAttrs.buildInputs ++ [ pkgs.perlPackages.IOSocketSSL ]; }))
+    flac
   ];
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
